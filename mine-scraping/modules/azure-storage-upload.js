7d6ab3304ff8babdce0209140a071eb05a9911bc
maktub82 <gallardo91_@hotmail.com>
2015-04-03 13:06:44 +0200

    Added online file support to Azure Storage

--- mine-scraping/modules/azure-storage-upload.js
+++ mine-scraping/modules/azure-storage-upload.js
@@ -1,8 +1,25 @@
 var azure = require('azure-storage');
-var blobSvc = azure.createBlobService();
-var containerName = "v1";
+var fs = require('fs');
+var request = require('request');
 
-var initilize = function() {
+var blobSvc = azure.createBlobService();
+var containerName = "minecraftv1";
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+// Enviroment variables
+// AZURE_STORAGE_ACCOUNT and
+// AZURE_STORAGE_ACCESS_KEY or
+// AZURE_STORAGE_CONNECTION_STRING 
+
+var initialize = function() {
 	// publicAccessLevel: blob: Allow anonymous access to blob but no container metadata or blob list.
 	blobSvc.createContainerIfNotExists(containerName, {publicAccessLevel : 'blob'}, function(error, result, response){
 	  if(!error){
@@ -12,12 +29,18 @@ var initilize = function() {
 }
 
 var uploadFile = function(name, file) {
-	blobSvc.createBlockBlobFromLocalFile(containerName, name, file, function(error, result, response){
-	  if(!error){
-	    // file uploaded
-	  }
+	download(file, name, function () {
+		uploadLocalFile(name, file);
+	});
+}
+
+var uploadLocalFile = function(name, file) {
+	blobSvc.createBlockBlobFromLocalFile(containerName, name, name, function(error, result, response){
+  		if(error){
+    		console.log(response);
+  		}
 	});
 }
 
 // 
-module.exports = {initilize: initilize, uploadFile: uploadFile};
+module.exports = {initialize: initialize, uploadFile: uploadFile, uploadLocalFile: uploadLocalFile};
