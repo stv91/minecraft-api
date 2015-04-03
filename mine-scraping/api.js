66fb1dbe29224e89e835d4fc6d7376cbfe53de48
maktub82 <gallardo91_@hotmail.com>
2015-04-04 00:59:45 +0200

    Added version information

--- mine-scraping/api.js
+++ mine-scraping/api.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var azure_sql = require("./modules/azure-sql");
 var cors = require('cors');
+var settings = require('./modules/settings');
+
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -13,6 +15,10 @@ app.get('/api/items', function (req, res) {
 	var response = azure_sql.getAllItems(function(err, recordset) {
 		res.json(recordset);
 	});
+});
+
+app.get('/api/version', function (req, res) {
+	res.send("Version " + process.env.VERSION + " USER:" + settings.db_user);
 });
 
 app.listen(process.env.PORT || 3000);
