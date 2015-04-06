02674b939775845f77b5bfc0976433bc898ca4ce
maktub82 <gallardo91_@hotmail.com>
2015-04-06 17:49:34 +0200

    Changed PORT and API pagination

--- api.js
+++ api.js
@@ -13,7 +13,7 @@ app.use(cors());
 //Routing
 app.get('/api/items/:page?', function (req, res) {
 	var page = req.params.page || 0;
-	page = Number.isInteger(page) ? page : 0;
+	//page = Number.isInteger(page) ? page : 0;
 	var response = azure_sql.getAllItems(page, function(err, recordset) {
 		res.json(recordset);
 	});
@@ -23,7 +23,7 @@ app.get('/api/version', function (req, res) {
 	res.send("Version " + process.env.VERSION + " USER:" + settings.db_user);
 });
 
-var port = 3000;
-app.listen(process.env.PORT || port);
+var port = parseInt(process.env.PORT) || 3000;
+app.listen(port);
 
 console.log('Api started. Port ' + port);
